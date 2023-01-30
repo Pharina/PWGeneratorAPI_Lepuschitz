849b3459cf59047a7c1676b45c514e77370ba3a9
Stephanie Lepuschitz <ic21b013@technikum-wien.at>
2023-01-30 17:58:45 +0100

    Initial commit

---
+++ src/main/java/at/fhtw/bic/pwgeneratorapi_lepuschitz/PwGeneratorApiLepuschitzApplication.java
@@ -0,0 +1,13 @@
+package at.fhtw.bic.pwgeneratorapi_lepuschitz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwGeneratorApiLepuschitzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PwGeneratorApiLepuschitzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/fhtw/bic/pwgeneratorapi_lepuschitz/PwGeneratorApiLepuschitzApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhtw.bic.pwgeneratorapi_lepuschitz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PwGeneratorApiLepuschitzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
